d65ac1acd6ec91dea5c7a9adf80588155b51074c
SamLindaman <46086020+SamLindaman@users.noreply.github.com>
2019-07-05 17:14:17 +0800

    Final Time Series Based Model
    Finished Arima Prediction

--- Arima.py
+++ Arima.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pylab as plt
 import statsmodels.api as sm
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.api import qqplot
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
@@ -43,16 +44,31 @@ dta_min = np.array(dta_min, dtype=np.float)
 dta_min = pd.Series(dta_min)
 dta_min.index = pd.Index(sm.tsa.datetools.dates_from_range(str(begin_year.values[0]), str(end_year.values[0])))
 
-dta_min.plot(figsize=(10, 6))
+Arma_mod39 = sm.tsa.ARMA(dta_min, (0, 4)).fit()
+print(Arma_mod39.aic, Arma_mod39.bic, Arma_mod39.hqic)
+resid = Arma_mod39.resid
 fig = plt.figure(figsize=(10, 6))
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_acf(dta_min, lags=30, ax=ax1)
-fig = sm.graphics.tsa.plot_pacf(dta_min, lags=30, ax=ax2)
+fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=30, ax=ax1)
+fig = sm.graphics.tsa.plot_pacf(resid, lags=30, ax=ax2)
+
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111)
+fig = qqplot(resid, line='q', ax=ax, fit = True)
+plt.show()
 
 diff1 = dta_min.diff(1)
 # diff1.plot(ax=ax1)
 plt.show()
+
+predict_year = 10
+
+predict_end_year = end_year.values[0] + predict_year
+predict_dta = Arma_mod39.predict(str(end_year.values[0]), str(predict_end_year), dynamic=True)
+print(predict_dta)
+
+
 
 
 
